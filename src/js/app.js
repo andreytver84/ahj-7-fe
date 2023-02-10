b1d53973d4088e2c94b4c598f971795ccc20400a
Андрей Увиков <uvikmedium@mail.ru>
2023-02-10 17:02:48 +0300

    work from lecture is done

--- src/js/app.js
+++ src/js/app.js
@@ -3,16 +3,20 @@ const subscribeForm = subscribeWidget.querySelector('.subscribe-form');
 const nameInput = subscribeWidget.querySelector('.name');
 const phoneInput = subscribeWidget.querySelector('.phone');
 const unsubscribeBtn = subscribeWidget.querySelector('.unsubscribe-btn');
+const uploadForm = document.querySelector('.upload-form');
+const previewImage = document.querySelector('.preview-image');
 
 subscribeForm.addEventListener('submit', (e) => {
     e.preventDefault(); 
 
     const xhr = new XMLHttpRequest();
 
-    const body = Array.from(subscribeForm.elements)
+    const body = new FormData(subscribeForm);
+
+   /*  const body = Array.from(subscribeForm.elements)
     .filter( ({name}) => name)
     .map(({name, value}) => `${name}=${encodeURIComponent(value)}`)
-    .join('&');
+    .join('&'); */
 
     //console.log(body);
 
@@ -24,7 +28,7 @@ subscribeForm.addEventListener('submit', (e) => {
 
     xhr.open('POST', 'http://localhost:7070');
 
-    xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+   /*  xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded'); */
 
     xhr.send(body);
 });
@@ -52,4 +56,27 @@ unsubscribeBtn.addEventListener('click', (e) => {
     xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
 
     xhr.send();
+});
+
+uploadForm.addEventListener('submit', (e) => {
+    e.preventDefault(); 
+    const xhr = new XMLHttpRequest();
+
+    const body = new FormData(uploadForm);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState !== 4) return;
+
+        //console.log(xhr.responseText);
+    }
+
+    xhr.open('POST', 'http://localhost:7070/upload');
+
+    xhr.send(body);
+
+    setTimeout(() => {
+        console.log(xhr.responseText)
+        previewImage.src = 'http://localhost:7070' + xhr.responseText;
+    }, 1000)
+
 });
