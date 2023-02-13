244eb7642a80b46ed34a6df75b612fd7e636b24a
Андрей Увиков <uvikmedium@mail.ru>
2023-02-13 19:28:07 +0300

    it is working

--- src/js/app.js
+++ src/js/app.js
@@ -1,82 +1,111 @@
-const subscribeWidget = document.querySelector('.subscribe');
-const subscribeForm = subscribeWidget.querySelector('.subscribe-form');
-const nameInput = subscribeWidget.querySelector('.name');
-const phoneInput = subscribeWidget.querySelector('.phone');
-const unsubscribeBtn = subscribeWidget.querySelector('.unsubscribe-btn');
-const uploadForm = document.querySelector('.upload-form');
-const previewImage = document.querySelector('.preview-image');
+const ticketsBlock = document.querySelector(".tickets");
+const addTicket = document.querySelector(".add-ticket");
+const modalAddTicket = document.querySelector(".modal-add-ticket");
+const formAddTicket = modalAddTicket.querySelector(".add-form");
+const btnAddTicket = formAddTicket.querySelector(".btn-add-ticket");
+const servUrl = "http://localhost:5050";
 
-subscribeForm.addEventListener('submit', (e) => {
-    e.preventDefault(); 
+const showModal = function (modal) {
+  modal.classList.remove("hidden");
+};
+const closeModal = function (modal) {
+  modal.classList.add("hidden");
+};
 
+const responseXhr = function (method, url, body = null) {
+  return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
 
-    const body = new FormData(subscribeForm);
+    xhr.onreadystatechange = function () {
+      if (this.readyState !== 4) return;
 
-   /*  const body = Array.from(subscribeForm.elements)
-    .filter( ({name}) => name)
-    .map(({name, value}) => `${name}=${encodeURIComponent(value)}`)
-    .join('&'); */
+      resolve(xhr.response);
+    };
 
-    //console.log(body);
+    xhr.open(method, url);
 
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState !== 4) return;
+    //xhr.responseType = 'json';
+    //xhr.setRequestHeader('Content-Type','application/json');
 
-        //console.log(xhr.responseText);
-    }
+    xhr.send(JSON.stringify(body));
+  });
+};
 
-    xhr.open('POST', 'http://localhost:7070');
-
-   /*  xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded'); */
-
-    xhr.send(body);
+addTicket.addEventListener("click", (e) => {
+  showModal(modalAddTicket);
 });
 
-unsubscribeBtn.addEventListener('click', (e) => {
-    e.preventDefault(); 
-
-    const xhr = new XMLHttpRequest();
-
-    const body = Array.from(subscribeForm.elements)
-    .filter( ({name}) => name)
-    .map(({name, value}) => `${name}=${encodeURIComponent(value)}`)
-    .join('&');
-
-    //console.log(body);
-
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState !== 4) return;
-
-        //console.log(xhr.responseText);
-    }
-
-    xhr.open('DELETE', 'http://localhost:7070/?' + body);
-
-    xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-
-    xhr.send();
+formAddTicket.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains("modal-close")) {
+    closeModal(e.target.closest(".modal"));
+  }
 });
 
-uploadForm.addEventListener('submit', (e) => {
-    e.preventDefault(); 
-    const xhr = new XMLHttpRequest();
+const tickets = document.querySelector(".tickets");
 
-    const body = new FormData(uploadForm);
+const renderTickets = function (arr) {
+  ticketsBlock.innerHTML = "";
+  arr.forEach((ticket) => {
+    const status = ticket.status ? "checked" : "";
+    const newTicket = `<div class="ticket" data-id="${ticket.id}">
+        <span class="ticket-checkbox"><input type="checkbox" ${status}></span>
+        <span class="ticket-name">${ticket.name}</span>
+        <span class="ticket-date">${ticket.created}</span>
+        <div class="ticket-manage">
+          <span class="ticket-edit"><button class="btn btn-edit">edit</button></span>
+          <span class="ticket-remove"><button class="btn btn-remove">remove</button></span>
+        </div>
+      </div>`;
+    tickets.insertAdjacentHTML("beforeEnd", newTicket);
+  });
+};
 
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState !== 4) return;
+responseXhr("GET", servUrl).then((data) => renderTickets(JSON.parse(data)));
 
-        //console.log(xhr.responseText);
-    }
+btnAddTicket.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalAddTicket.querySelector("h3").textContent = "Добавить тикет";
+  const formdata = new FormData(formAddTicket);
+  const data = Object.fromEntries(formdata.entries());
+  if (modalAddTicket.dataset.id) {
+    data.id = modalAddTicket.dataset.id;
+    data.status = modalAddTicket.dataset.checked;
+    console.log(data);
+  }
+  const datajson = JSON.stringify(data);
 
-    xhr.open('POST', 'http://localhost:7070/upload');
+  responseXhr("POST", servUrl, datajson).then((data) =>
+    renderTickets(JSON.parse(data))
+  );
+  //responseXhr("POST", servUrl,datajson).then(data => console.log(data));
+  closeModal(e.target.closest(".modal"));
+  modalAddTicket.dataset.id = "";
+  modalAddTicket.dataset.checked = "";
+  formAddTicket.reset();
+});
 
-    xhr.send(body);
-
-    setTimeout(() => {
-        console.log(xhr.responseText)
-        previewImage.src = 'http://localhost:7070' + xhr.responseText;
-    }, 1000)
-
+ticketsBlock.addEventListener("click", (e) => {
+  let curEl = e.target;
+  if (curEl.classList.contains("btn-remove")) {
+    const id = curEl.closest(".ticket").dataset.id;
+    responseXhr("DELETE", servUrl + "/?id=" + id, id).then((data) =>
+      renderTickets(JSON.parse(data))
+    );
+  }
+  if (curEl.classList.contains("btn-edit")) {
+    showModal(modalAddTicket);
+    modalAddTicket.querySelector("h3").textContent = "Редактировать тикет";
+    const id = curEl.closest(".ticket").dataset.id;
+    const checked = curEl.closest(".ticket").querySelector("input").checked
+      ? true
+      : false;
+    modalAddTicket.dataset.id = id;
+    modalAddTicket.dataset.checked = checked;
+    responseXhr("GET", servUrl).then((data) => {
+      const cur = JSON.parse(data).filter((obj) => obj.id == id);
+      modalAddTicket.querySelector("input").value = cur[0].name;
+      modalAddTicket.querySelector("textarea").value = cur[0].description;
+    });
+  }
 });
